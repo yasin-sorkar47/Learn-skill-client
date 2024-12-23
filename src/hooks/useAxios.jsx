172e5a65e57e2a382646a7ce91e6f1d03fa242c3
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-12-23 20:59:14 +0600

    used the varifycation with manage service page

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,8 +1,32 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
 export default function useAxios() {
+  const { singOutUser, setLoader } = useAuth();
+  const navigate = useNavigate();
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401 || error.response.status === 403) {
+        singOutUser();
+        then(() => {
+          setLoader(false);
+          navigate("/login");
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
   return axiosInstance;
 }
--- src/pages/ManageService.jsx
+++ src/pages/ManageService.jsx
@@ -4,21 +4,18 @@ import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import ManageServiceCard from "../components/ManageServiceCard";
 import useAuth from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
 
 const ManageServices = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
+  const axiosInstance = useAxios();
   const [services, setServices] = useState([]);
 
   useEffect(() => {
     // Fetch services from the API
     async function fetchData() {
-      const { data } = await axios.get(
-        `http://localhost:5000/services/${user.email}`,
-        {
-          withCredentials: true,
-        }
-      );
+      const { data } = await axiosInstance.get(`/services/${user.email}`);
       setServices(data);
     }
 
@@ -61,41 +58,41 @@ const ManageServices = () => {
 
   return (
     <div className="min-h-screen  flex  justify-center">
-      {services.length > 0 ? (
-        <div className=" p-8 rounded-lg  w-full max-w-4xl">
-          <h1 className="text-2xl lg:text-3xl font-bold dark:text-gray-100 text-gray-800 text-center">
-            My added Services{" "}
-            <span className="bg-purple-300 px-3 text-purple-500 text-xl rounded-full">
-              {services.length}
-            </span>
-          </h1>
-          <p className="mb-8 mt-2 text-center text-gray-600 dark:text-gray-300 mx-auto max-w-[700px]">
-            Enthusiastically maximize virtual methodologies whereas
-            backward-compatible e-services. Quickly develop principle-centered
-            scenarios before next-generation action items
-          </p>
-          <div className="space-y-6">
-            {services.map((service) => (
+      <div className=" p-8 rounded-lg  w-full max-w-4xl">
+        <h1 className="text-2xl lg:text-3xl font-bold dark:text-gray-100 text-gray-800 text-center">
+          My added Services{" "}
+          <span className="bg-purple-300 px-3 text-purple-500 text-xl rounded-full">
+            {services.length}
+          </span>
+        </h1>
+        <p className="mb-8 mt-2 text-center text-gray-600 dark:text-gray-300 mx-auto max-w-[700px]">
+          Enthusiastically maximize virtual methodologies whereas
+          backward-compatible e-services. Quickly develop principle-centered
+          scenarios before next-generation action items
+        </p>
+        <div className="space-y-6">
+          {services.length > 0 ? (
+            services.map((service) => (
               <ManageServiceCard
                 key={service._id}
                 service={service}
                 handleDelete={handleDelete}
               />
-            ))}
-          </div>
+            ))
+          ) : (
+            <div className="text-center text-gray-800 dark:text-gray-100">
+              <p>You have not added any services yet.</p>
+              <p>Click the button below to add a new service.</p>
+              <button
+                className="btn btn-primary btn-md mt-4"
+                onClick={() => navigate("/addService")}
+              >
+                Add Service
+              </button>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="text-center text-gray-800 dark:text-gray-100">
-          <p>You have not added any services yet.</p>
-          <p>Click the button below to add a new service.</p>
-          <button
-            className="btn btn-primary btn-md mt-4"
-            onClick={() => navigate("/addService")}
-          >
-            Add Service
-          </button>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
